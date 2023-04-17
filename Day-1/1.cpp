a1045327aebd25dd6e640d05158290cc3e4fc0db
Tejaswi Chaudhari <62535464+Tejaswi-Chaudhari@users.noreply.github.com>
2023-04-17 17:31:11 +0530

    Solution-1

---
+++ Day-1/1.cpp
@@ -0,0 +1,18 @@
+// The brute-force method
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ind;
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if(nums[i]+nums[j]==target){
+                    ind.push_back(i);
+                    ind.push_back(j);
+                    break;
+                }
+            }
+        }
+        return ind;
+    }
+};
