7956ad311df52edf66c95964c9818868670f1583
Tejaswi Chaudhari <62535464+Tejaswi-Chaudhari@users.noreply.github.com>
2023-04-28 18:59:13 +0530

    Create 1.cpp

---
+++ Day-12/1.cpp
@@ -0,0 +1,19 @@
+// Binary Search Method
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int i=0;
+        int j=nums.size()-1;
+        while (i <= j) {
+            int mid = (i+j)/2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                i = mid + 1;
+            else
+                j = mid - 1;
+        }
+        return j+1;
+    }
+};
