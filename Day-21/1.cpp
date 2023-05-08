5a5e5d840bdce3f64516c3217c511637418fc3d2
Tejaswi Chaudhari <62535464+Tejaswi-Chaudhari@users.noreply.github.com>
2023-05-08 23:22:34 +0530

    Create 1.cpp

---
+++ Day-21/1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size()-1;
+        for (int i = 0; i < (n+1) / 2; i++) {
+            for (int j = i; j < n - i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j][i];
+                matrix[n - j][i] = matrix[n - i][n - j];
+                matrix[n - i][n - j] = matrix[j][n - i];
+                matrix[j][n - i] = temp;
+            }
+        }
+    }
+};
