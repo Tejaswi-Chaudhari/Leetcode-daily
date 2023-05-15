2408f141c06c337623d745094d04ba53445e6e17
Tejaswi Chaudhari <62535464+Tejaswi-Chaudhari@users.noreply.github.com>
2023-05-15 16:41:25 +0530

    Create 1.cpp

---
+++ Day-27/1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans=intervals;
+        sort(ans.begin(), ans.end());
+        do{
+            intervals=ans;
+            for(int i=0; i<ans.size()-1; i++){
+                if(ans[i][1]<ans[i+1][0]){
+                    continue;
+                }
+                ans[i][0]=min(min(ans[i][0], ans[i][1]), min(ans[i+1][0], ans[i+1][1]));
+                ans[i][1]=max(max(ans[i][0], ans[i][1]), max(ans[i+1][0], ans[i+1][1]));
+                ans.erase(ans.begin() + (i+1));
+            }
+        }
+        while(!(ans==intervals));
+        return ans;
+    }
+};
